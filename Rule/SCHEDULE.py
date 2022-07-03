76f2657017d6d9f3d8d23eb8ba988babb64d1886
Kim Hansol <khs_9902@naver.com>
2022-07-03 21:38:59 +0900

    Fix Schedule <Machine hard coding>

--- Rule/SCHEDULE.py
+++ Rule/SCHEDULE.py
@@ -1,21 +1,3 @@
-from Schedule_1 import SCHEDULE
-
-List_1 = ['LOT_1-A_1', 'LOT_2-B_1', 'LOT_1-A_2', 'LOT_1-A_3', 'LOT_3-B_1', 'LOT_3-B_2', 'LOT_2-B_2']
-
-Schedule = {'M_1': [],
-            'M_2': [],
-            'M_3': [],
-            'M_4': []}
-
-PROCESSING_TIME = {'A_1': 4, 'A_2': 4, 'B_1': 5, 'A_3': 1, 'B_2': 2, 'C_1': 3, 'C_2': 3}
-INITIAL_SETUP = {'M_1': 'A_1', 'M_2': 'A_1', 'M_3': 'A_1', 'M_4': 'A_1'}
-
-SETUP_TIME = {'homogeneous_setup': 2, 'heterogeneous_setup': 4}
-
-first = SCHEDULE(Schedule, List_1, PROCESSING_TIME, INITIAL_SETUP, SETUP_TIME)
-print(first.rule_1())
-
-
 class SCHEDULE:
     def __init__(self, Schedule, List_1, PROCESSING_TIME, INITIAL_SETUP, SETUP_TIME):
         self.Schedule = Schedule
@@ -27,10 +9,12 @@ class SCHEDULE:
     def rule_1(self):
         # 기계 시간 초기화
         machine_time = []
+        machine_name = []
         for machine in self.Schedule.keys():
             machine_time.append(0)
+            machine_name.append(machine)
         # print('#Machine Time Reset', machine_time)
-
+        print(machine_name)
         pct_list = []
         for i in self.List_1:
             pct = i.split("-")[1]
@@ -49,47 +33,47 @@ class SCHEDULE:
         for work in self.List_1:
 
             if next_machine_index == 0:
-                self.Schedule['M_1'].append(work)
-                if self.INITIAL_SETUP["M_1"] == work.split("-")[1]:  # 셋업 시간 계산
+                self.Schedule[machine_name[0]].append(work)
+                if self.INITIAL_SETUP[machine_name[0]] == work.split("-")[1]:  # 셋업 시간 계산
                     pass
-                elif self.INITIAL_SETUP["M_1"][0] == work.split("-")[1][0]:
+                elif self.INITIAL_SETUP[machine_name[0]][0] == work.split("-")[1][0]:
                     machine_time[next_machine_index] += self.SETUP_TIME['homogeneous_setup']
                 else:
                     machine_time[next_machine_index] += self.SETUP_TIME['heterogeneous_setup']
-                self.INITIAL_SETUP['M_1'] = pct_list[0]
+                self.INITIAL_SETUP[machine_name[0]] = pct_list[0]
                 machine_time[next_machine_index] += PCT[0]
 
             elif next_machine_index == 1:
-                self.Schedule['M_2'].append(work)
-                if self.INITIAL_SETUP["M_2"] == work.split("-")[1]:  # 셋업 시간 계산
+                self.Schedule[machine_name[1]].append(work)
+                if self.INITIAL_SETUP[machine_name[1]] == work.split("-")[1]:  # 셋업 시간 계산
                     pass
-                elif self.INITIAL_SETUP["M_2"][0] == work.split("-")[1][0]:
+                elif self.INITIAL_SETUP[machine_name[1]][0] == work.split("-")[1][0]:
                     machine_time[next_machine_index] += self.SETUP_TIME['homogeneous_setup']
                 else:
                     machine_time[next_machine_index] += self.SETUP_TIME['heterogeneous_setup']
-                self.INITIAL_SETUP['M_2'] = pct_list[0]
+                self.INITIAL_SETUP[machine_name[1]] = pct_list[0]
                 machine_time[next_machine_index] += PCT[0]
 
             elif next_machine_index == 2:
-                self.Schedule['M_3'].append(work)
-                if self.INITIAL_SETUP["M_3"] == work.split("-")[1]:  # 셋업 시간 계산
+                self.Schedule[machine_name[2]].append(work)
+                if self.INITIAL_SETUP[machine_name[2]] == work.split("-")[1]:  # 셋업 시간 계산
                     pass
-                elif self.INITIAL_SETUP["M_3"][0] == work.split("-")[1][0]:
+                elif self.INITIAL_SETUP[machine_name[2]][0] == work.split("-")[1][0]:
                     machine_time[next_machine_index] += self.SETUP_TIME['homogeneous_setup']
                 else:
                     machine_time[next_machine_index] += self.SETUP_TIME['heterogeneous_setup']
-                self.INITIAL_SETUP['M_3'] = pct_list[0]
+                self.INITIAL_SETUP[machine_name[2]] = pct_list[0]
                 machine_time[next_machine_index] += PCT[0]
 
             elif next_machine_index == 3:
-                self.Schedule['M_4'].append(work)
-                if self.INITIAL_SETUP["M_4"] == work.split("-")[1]:  # 셋업 시간 계산
+                self.Schedule[machine_name[3]].append(work)
+                if self.INITIAL_SETUP[machine_name[3]] == work.split("-")[1]:  # 셋업 시간 계산
                     pass
-                elif self.INITIAL_SETUP["M_4"][0] == work.split("-")[1][0]:
+                elif self.INITIAL_SETUP[machine_name[3]][0] == work.split("-")[1][0]:
                     machine_time[next_machine_index] += self.SETUP_TIME['homogeneous_setup']
                 else:
                     machine_time[next_machine_index] += self.SETUP_TIME['heterogeneous_setup']
-                self.INITIAL_SETUP['M_4'] = pct_list[0]
+                self.INITIAL_SETUP[machine_name[3]] = pct_list[0]
                 machine_time[next_machine_index] += PCT[0]
 
             del pct_list[0], PCT[0]
